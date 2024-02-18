1ad13000ea67e1560e88f32defe8c71f0e9b97ec
Lambru Adrian Georgian <120751654+lambrugeorge@users.noreply.github.com>
2024-02-18 22:03:58 +0200

    Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://mybackend-dfd1.onrender.com/3000', {
+    fetch('https://mybackend-dfd1.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
