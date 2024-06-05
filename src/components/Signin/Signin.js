f5ee1435eec5c8123ddf3900f95f54ebb2e7fe4f
Lambru Adrian Georgian <120751654+lambrugeorge@users.noreply.github.com>
2024-06-05 15:58:32 +0300

    Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,65 +1,100 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
-  const [signInEmail, setSignInEmail] = useState('');
-  const [signInPassword, setSignInPassword] = useState('');
-
-  const onSubmitSignIn = () => {
-    axios.post('http://localhost:3000/signin', {
-      email: signInEmail,
-      password: signInPassword
-    })
-    .then(response => {
-      if (response.data === 'success') {
-        onRouteChange('home');
-      }
-    })
-    .catch(err => console.log(err));
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
   }
 
-  return (
-    <article className="br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main className="pa4 white-80">
-        <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  }
+
+  onSubmitSignIn = () => {
+    fetch('https://rainbow-meerkat-35462e.netlify.app/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user);
+          fetch('https://rainbow-meerkat-35462e.netlify.app/getimages', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: user.id
+            })
+          })
+            .then(response => response.json())
+            .then(images => {
+              if (images) {
+                console.log(images);
+                this.props.loadImages(images);
+              }
+            });
+          this.props.onRouteChange('home');
+        }
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100"
-                type="email"
-                name="email-address"
-                id="email-address"
-                onChange={e => setSignInEmail(e.target.value)}
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="button"
+                value="Sign in"
               />
             </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input
-                className="b pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100"
-                type="password"
-                name="password"
-                id="password"
-                onChange={e => setSignInPassword(e.target.value)}
-              />
+            <div className="lh-copy mt3">
+              <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
-          </fieldset>
-          <div className="">
-            <input
-              onClick={onSubmitSignIn}
-              className="b ph3 pv3 input-reset ba b--white-smoke bg-transparent grow pointer f6 dib white"
-              type="submit"
-              value="Sign in"
-            />
           </div>
-          <div className="lh-copy mt3">
-            <p  onClick={() => onRouteChange('register')} className="f6 link dim white db pointer">Register</p>
-          </div>
-        </div>
-      </main>
-    </article>
-  );
+        </main>
+      </article>
+    );
+  }
 }
 
 export default Signin;
